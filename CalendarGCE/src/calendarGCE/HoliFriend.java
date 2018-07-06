0ef3ef32b48a13e6edfe3aeafc2da9dd1863a4ed
gce <gce@LVB9R49H2>
2018-07-06 08:30:48 -0400

    final commit

--- CalendarGCE/src/calendarGCE/HoliFriend.java
+++ CalendarGCE/src/calendarGCE/HoliFriend.java
@@ -8,102 +8,6 @@ import java.util.GregorianCalendar;
 
 public class HoliFriend{
     
-
-//*********************************
-// Miscellaneous other holidays are left as an exercise for the reader.
-//
-// public static Date QuebecCivicHoliday (int nYear)
-// {
-//  // 03 January YYYY
-// }
-//
-// public static Date AshWednesday (int nYear)
-// {
-//  // 42 days before easter...
-// }
-//
-// public static Date PalmSunday (int nYear)
-// {
-//  // Sunday before Easter Sunday...
-// }
-//
-// public static Date MaundayThursday (int nYear)
-// {
-//  // Thursday before Easter...
-// }
-//
-//  public static Date RoshHashanah(int nYear)
-// {
-//  Source: William H. Jefferys, Department of Astronomy, University of
-//  Texas Austin, TX 78712 
-//
-//         http://quasar.as.utexas.edu
-//
-//  First, calculate the Golden Number G. This is fundamental to the
-//  calculation of both the date of Easter and the Date of Rosh Hashanah. 
-//  It is intimately connected with the Metonic Cycle. For any year Y, the
-//  Golden Number is defined as 
-//
-//  G = Remainder(Y|19) + 1. Don't forget to add the 1!!!
-//
-//  The following rules are also due to John Horton Conway, of Princeton
-//  University. In the Gregorian year Y of the Common Era, Rosh Hashanah
-//  normally falls on September N, where
-//
-//  N + fraction = {[Y/100] - [Y/400] - 2} +
-//  765433/492480*Remainder(12G|19) + Remainder(Y|4)/4 - (313Y+89081)/98496
-//
-//  Here, G is the Golden Number, and * means multiply. However, if certain
-//  conditions are satisfied, Rosh Hashanah is postponed by one or even two
-//  days, as follows: 
-//
-//      ***Postponement rules***
-//
-//  1.If the day calculated above is a Sunday, Wednesday, or Friday, Rosh
-//  Hashanah falls on the next day (Monday, Thursday or Saturday,
-//  respectively).
-//
-//  2.If the calculated day is a Monday, and if the fraction is greater
-//  than or equal to 23269/25920, and if Remainder(12G|19) is greater than
-//  11, Rosh Hashanah falls on the next day, a Tuesday.
-//
-//  3.If it is a Tuesday, and if the fraction is greater than or equal to
-//  1367/2160, and if Remainder(12G|19) is greater than 6, Rosh Hashanah
-//  falls two days later, on Thursday (NOT WEDNESDAY!!).
-// }
-//
-//  public static Date Passover(int nYear)
-// {
-//  Source: William H. Jefferys, Department of Astronomy, University of
-//  Texas Austin, TX 78712
-//
-//         http://quasar.as.utexas.edu
-//
-//  Once you have determined the date of Rosh Hashanah, it is easy to
-//  calculate the date of Passover in the same (Gregorian or Julian)
-//  year. Let M = the number of days from September 6 to Rosh Hashanah.
-//  In the example for 1996, M=September 14-September 6 = 8 days.
-//
-//  Count M days from March 27. That is the date of Passover. It actually
-//  begins at sundown on the previous evening. In the example for 1996, 8
-//  days after March 27 is April 4 (there are 31 days in March), so
-//  Passover begins at sundown on April 3.
-// }
-//
-// public static Date DominionDay (int nYear)
-// {
-//  // 01 July YYYY
-// }
-//
-// public static Date BoxingDay (int nYear)
-//  {
-//  // Day after Christmas, December 26th...
-//  }
-//
-//*********************************************
-
-
-    
 public static java.util.Calendar AbrahamLincolnsBirthday (int nYear){
   int nMonth = 1; // February
   // February 12th
@@ -220,26 +124,7 @@ public static java.util.Calendar ChristmasDay (int nYear){
   }
     public static String EasterMonday (int nYear)
   {
-    	/*
-    	SimpleDateFormat sdf = new SimpleDateFormat("yyyy MMM dd HH:mm:ss");	
-  int nEasterMonth = 0;
-  int nEasterDay   = 0;
-  int nMonthMarch  = 2; // March
-  int nMonthApril  = 3; // April
-  java.util.Calendar cEasterSunday = EasterSunday(nYear);
-  nEasterMonth = cEasterSunday.get(Calendar.MONTH);
-  nEasterDay = cEasterSunday.get(Calendar.DAY_OF_MONTH);
-  if (nEasterMonth == nMonthMarch || nEasterDay == 31){
-    java.util.Calendar cal = java.util.Calendar.getInstance();
-    cal.set(nYear, nMonthApril, 1);
-    System.out.println(sdf.format(cal.getTime()));
-    return cal;
-  }else{
-    java.util.Calendar cal = java.util.Calendar.getInstance();
-    cal.set(nYear, nEasterMonth, ++nEasterDay);
-    System.out.println(sdf.format(cal.getTime()));
-    return cal;
-    */
+    	
     	 int a = nYear % 19,
     	            b = nYear / 100,
     	            c = nYear % 100,
@@ -306,40 +191,6 @@ public static java.util.Calendar ChristmasDay (int nYear){
 
     public static java.util.Calendar EasterSunday(int nYear)
   { 
-/*  Calculate Easter Sunday
-
-  Written by Gregory N. Mirsky
-
-  Source: 2nd Edition by Peter Duffett-Smith. It was originally from
-  Butcher's Ecclesiastical Calendar, published in 1876. This
-  algorithm has also been published in the 1922 book General
-  Astronomy by Spencer Jones; in The Journal of the British
-  Astronomical Association (Vol.88, page 91, December 1977); and in
-  Astronomical Algorithms (1991) by Jean Meeus.
-
-  This algorithm holds for any year in the Gregorian Calendar, which
-  (of course) means years including and after 1583.
-
-        a=year%19
-        b=year/100
-        c=year%100
-        d=b/4
-        e=b%4
-        f=(b+8)/25
-        g=(b-f+1)/3
-        h=(19*a+b-d-g+15)%30
-        i=c/4
-        k=c%4
-        l=(32+2*e+2*i-h-k)%7
-        m=(a+11*h+22*l)/451
-        Easter Month =(h+l-7*m+114)/31  [3=March, 4=April]
-        p=(h+l-7*m+114)%31
-        Easter Date=p+1     (date in Easter Month)
-
-  Note: Integer truncation is already factored into the
-  calculations. Using higher percision variables will cause
-  inaccurate calculations. 
-*/
 
   int nA      = 0;
   int nB      = 0;
